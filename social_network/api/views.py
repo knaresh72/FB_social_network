98240d45c715389c8bf68af98088386276403e3c
knaresh72 <jknareshdeveloper@gmail.com>
2023-12-26 12:39:18 +0530

    Your message

--- social_network/api/views.py
+++ social_network/api/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from .models import UserProfile, FriendRequest, Friendship
@@ -10,14 +11,14 @@ from .serializers import UserProfileSerializer, FriendRequestSerializer, Friends
 from rest_framework.decorators import api_view, permission_classes
 
 
-# @api_view(['POST'])
-# @permission_classes([AllowAny])
-# def user_signup(request):
-#     serializer = UserProfileSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def user_signup(request):
+    serializer = UserProfileSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
